80c02115768d7ec48c5ad37648498666c77ce82d
zelvaiz <zelvaimam15@gmail.com>
2024-05-01 16:56:42 +0700

    pengerjaan ulang task 1

--- TASK-1/TASK-1.py
+++ TASK-1/TASK-1.py
@@ -4,11 +4,12 @@ import pandas as pd
 path = r'C:\Users\Axioo Pongo\Documents\Alterra\ingestion-data\dataset\yellow_tripdata_2020-07.csv'
 df = pd.read_csv(path, sep=',')
 
-df_rename = df.rename(columns= {"VendorID":"Vendor_ID","RatecodeID":"Rate_Code_ID",
-                                "PULocationID":"PULocation_ID",
-                                "DOLocationID":"DOLocation_ID"})
+df_rename = df.rename(columns= {"VendorID":"vendor_id",
+                                "RatecodeID":"rate_code_id",
+                                "PULocationID":"pu_location_id",
+                                "DOLocationID":"do_location_id"})
 
-bottom_10_passenger = df_rename.nsmallest(10, 'passenger_count') [['Vendor_ID', 'passenger_count', 'trip_distance', 'payment_type', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 
+bottom_10_passenger = df_rename.nsmallest(10, 'passenger_count') [['vendor_id', 'passenger_count', 'trip_distance', 'payment_type', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 
                      'tolls_amount', 'improvement_surcharge', 'total_amount', 'congestion_surcharge']]
 
 print(bottom_10_passenger)
